c0ae21ed26b02d61e6751f6e6f447c1e02e96b1e
Justin Jiang <jus3472@gmail.com>
2025-11-03 13:52:28 -0600

    Initial commit

---
+++ Smart Notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smart Notes
+//
+//  Created by Justin Jiang on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("This is the start of Smart Notes")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smart Notes/Smart_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_NotesApp.swift
+//  Smart Notes
+//
+//  Created by Justin Jiang on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
